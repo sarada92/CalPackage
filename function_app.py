5dcab565434a1758e310bf9394e690ad90f64a51
Sarada Rout <srout@groupon.com>
2023-05-14 18:04:24 +0530

    Add simple

--- function_app.py
+++ function_app.py
@@ -1,6 +1,6 @@
 import azure.functions as func
 import logging
-from ADD import ADD
+# from ADD import ADD
 
 app = func.FunctionApp()
 
@@ -9,14 +9,36 @@ app = func.FunctionApp()
 # Get started by running the following code to create a function using a HTTP trigger.
 
 
-@app.function_name(name="calculatorApp")
-@app.route(route="add")
+# @app.function_name(name="calculatorApp")
+# @app.route(route="add")
+# def test_function(req: func.HttpRequest) -> func.HttpResponse:
+#      logging.info('Python HTTP trigger function processed a request.')
+
+#      val1 = req.params.get('val1')
+#      val2 = req.params.get('val2')
+#      summ = ADD().addition(val1, val2)
+
+#     return func.HttpResponse(f"Hello, {summ}. This HTTP triggered function executed successfully.")
+    
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
      logging.info('Python HTTP trigger function processed a request.')
 
-     val1 = req.params.get('val1')
-     val2 = req.params.get('val2')
-     summ = ADD().addition(val1, val2)
+     name = req.params.get('name')
+     if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
 
-    return func.HttpResponse(f"Hello, {summ}. This HTTP triggered function executed successfully.")
-    
+     if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+     else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
